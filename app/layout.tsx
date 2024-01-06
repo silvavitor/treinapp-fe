b47cf5b903e5a0d6fb0f0f80868477ddb872514c
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-06 15:50:26 -0300

    feat: add style to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + ' max-w-3xl m-auto'}>
+        <div className='mx-2'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }

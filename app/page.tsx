2e019ef54266af8f2275e3ae4a4e8685ec15e0a6
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-06 15:51:44 -0300

    feat: add tabs with page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,26 @@
-"use client";
-
-import TrainingSummaryCard from "@/components/custom/trainingSummaryCard";
-import { useCallback, useEffect, useState } from "react";
-
-type Athlete = {
-  id: string,
-  name: string
-}
-
-type Exercise = {
-    id: string;
-    name: string;
-    sets_qtd: number,
-    trainingId: string;
-    createdAt: Date,
-    updatedAt: Date,
-}
-
-type Training = {
-    id: string,
-    name: string,
-    createdAt: Date,
-    updatedAt: Date,
-    athletes: Athlete[],
-    exercises: Exercise[],
-    _count: {
-        exercises: number,
-        athletes: number,
-        TrainingExecution: number
-    }
-}
+import Exercises from "@/components/pages/exercises";
+import Trainings from "@/components/pages/trainings";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
-  const [trainings, setTrainings] = useState<Training[]>([]);
-
-  const fetchTraining = useCallback(async () => {
-    const response =  await fetch('http://localhost:3000/trainings');
-    const trainingsJson = await response.json();
-
-    setTrainings(trainingsJson);
-  }, []);
-
-  useEffect(() => {
-    fetchTraining();
-  }, [fetchTraining]);
-
   return (
-    <>
-      {trainings.map((training, i) => (
-        <TrainingSummaryCard key={i} title={training.name} exercises={training._count.exercises}/>
-      ))}
-    </>
+    <div>
+      <header className="my-8">
+        <Tabs defaultValue="trainings" className="w-full mb-4">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="trainings">Treinos</TabsTrigger>
+            <TabsTrigger value="exercises">Exercicios</TabsTrigger>
+            <TabsTrigger value="athletes">Atletas</TabsTrigger>
+          </TabsList>
+          <TabsContent value="trainings">
+            <Trainings />
+          </TabsContent>
+          <TabsContent value="exercises">
+            <Exercises />
+          </TabsContent>
+          <TabsContent value="athletes"></TabsContent>
+        </Tabs>
+      </header>
+    </div>
   );
 }

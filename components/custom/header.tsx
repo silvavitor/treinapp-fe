d6ba3aeb2fe7031b41951784468cb62737ba64c5
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-09 19:38:46 -0300

    feat: add buttons to header

--- components/custom/header.tsx
+++ components/custom/header.tsx
@@ -2,13 +2,26 @@ import Image from "next/image";
 
 import logo from "@/public/logo.png";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function Header() {
   return (
-    <header className="w-full flex justify-center items-center h-40">
+    <header className="w-full flex flex-col justify-center items-center h-40">
       <Link href="/">
         <Image alt="logo" src={logo}/>
       </Link>
+
+      <div className="w-full mt-8 flex justify-around">
+        <Link href="/">
+          <Button className="w-40 border border-stone-200" variant="secondary">Treinos</Button>
+        </Link>
+        <Link href="/exercises">
+          <Button className="w-40 border border-stone-200" variant="secondary">Exercícios</Button>
+        </Link>
+        <Link href="/athletes">
+          <Button className="w-40 border border-stone-200" variant="secondary">Atletas</Button>
+        </Link>
+      </div>
     </header>
   )
 }

34a176fcda09b5b745a4fa401e3759805dfb280a
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-09 19:39:01 -0300

    feat: remove tabs and add pages to each type

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,33 @@
-import Athletes from "@/components/pages/athletes";
-import Exercises from "@/components/pages/exercises";
-import Trainings from "@/components/pages/trainings";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+"use client"
+
+import TrainingSummaryCard from "@/components/custom/trainingSummaryCard";
+import { Button } from "@/components/ui/button";
+import { Training } from "@/types";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
+  const [trainings, setTrainings] = useState<Training[]>([]);
+
+  const fetchTraining = useCallback(async () => {
+    const response =  await fetch('http://localhost:3000/trainings');
+    const trainingsJson = await response.json();
+
+    setTrainings(trainingsJson);
+  }, []);
+
+  useEffect(() => {
+    fetchTraining();
+  }, [fetchTraining]);
+
   return (
     <div>
-      <header className="my-8">
-        <Tabs defaultValue="trainings" className="w-full mb-4">
-          <TabsList className="grid w-full grid-cols-3">
-            <TabsTrigger value="trainings">Treinos</TabsTrigger>
-            <TabsTrigger value="exercises">Exercicios</TabsTrigger>
-            <TabsTrigger value="athletes">Atletas</TabsTrigger>
-          </TabsList>
-          <TabsContent value="trainings">
-            <Trainings />
-          </TabsContent>
-          <TabsContent value="exercises">
-            <Exercises />
-          </TabsContent>
-          <TabsContent value="athletes">
-            <Athletes />
-          </TabsContent>
-        </Tabs>
-      </header>
+      <h1 className="font-bold text-3xl my-4 text-center">Treinos</h1>
+      <div className="flex justify-end items-end mb-4">
+        <Button>Adicionar</Button>
+      </div>
+      {trainings.map((training, i) => (
+        <TrainingSummaryCard key={i} title={training.name} exercises={training._count.exercises}/>
+        ))}
     </div>
   );
 }

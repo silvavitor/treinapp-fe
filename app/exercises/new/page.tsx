07fd77686b636e066a4ed0d639359b067c09759f
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-12 23:23:10 -0300

    feat: create new exercise

---
+++ app/exercises/new/page.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import * as z from "zod"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import Select, { SelectOptions } from "@/components/custom/select";
+import { useCallback, useEffect, useState } from "react";
+import { Training } from "@/types";
+ 
+const formSchema = z.object({
+  name: z.string(),
+  sets_qtd: z.coerce.number().min(1, {message: "deve ter no mínimo 1 série"}),
+  trainingId: z.string().uuid(),
+});
+
+export default function Exercise() {
+  const [options, setOptions] = useState<SelectOptions[]>([]);
+  const router = useRouter();
+
+  const fetchTrainings = useCallback(async () => {
+    const response = await fetch(`http://localhost:3000/trainings`);
+    const responseJson: Training[] = await response.json();
+    const options: SelectOptions[] = responseJson.map((training, i) => {
+      return {
+        value: training.id,
+        text: training.name,
+        selected: i === 0,
+      }
+    });
+
+    setOptions(options);
+  }, []);
+
+  useEffect(() => {
+    fetchTrainings();
+  }, [fetchTrainings]);
+  
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    await fetch(`http://localhost:3000/exercises`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: values.name,
+        sets_qtd: values.sets_qtd,
+        trainingId: values.trainingId,
+      })
+    });
+
+    toast.success('Exercicio criado com sucesso!');
+    router.push('/exercises');
+  }
+
+  function handleBack() {
+    router.push('/exercises');
+  }
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      sets_qtd: 0,
+      trainingId: "",
+    },
+  });
+
+  return (
+    <div className="mt-8">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome do exercicio</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nome do exercicio" {...field} {...form.register('name')} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <FormField
+            control={form.control}
+            name="sets_qtd"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quantidade de séries</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="Quantidade de séries" {...field} {...form.register('sets_qtd')} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <FormField
+            control={form.control}
+            name="trainingId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Treino</FormLabel>
+                  <FormControl>
+                    <Select onChange={field.onChange} options={options} />
+                  </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <div className="space-x-4">
+            <Button variant="secondary" type="button" onClick={handleBack}>Voltar</Button>
+            <Button type="submit">Criar</Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -4,6 +4,7 @@ import ExercisesSummaryCard from "@/components/custom/exercisesSummaryCard";
 import { useCallback, useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Exercise } from "@/types";
+import Link from "next/link";
 
 export default function Exercises() {
   const [exercises, setExercises] = useState<Exercise[]>([]);
@@ -23,7 +24,9 @@ export default function Exercises() {
     <>
       <h1 className="font-bold text-3xl my-4 text-center">Exercicios</h1>
       <div className="flex justify-end items-end mb-4">
-        <Button>Adicionar</Button>
+        <Link href="/exercises/new">
+          <Button>Adicionar</Button>
+        </Link>
       </div>
       {exercises.map((exercise, i) => (
         <ExercisesSummaryCard key={i} title={exercise.name} sets={exercise.sets_qtd} training={exercise.Training.name}/>

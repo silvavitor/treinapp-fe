aaf64f6a8d49ce6ddd995b9faf2126650080d308
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-16 21:52:42 -0300

    feat: start a training execution

--- components/custom/trainingSummaryCard.tsx
+++ components/custom/trainingSummaryCard.tsx
@@ -1,10 +1,14 @@
 import {
   Card,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+import { MouseEvent } from "react";
 
 type TrainingSummaryCardProps = {
   id: string;
@@ -14,15 +18,43 @@ type TrainingSummaryCardProps = {
 }
 
 export default function TrainingSummaryCard({ id, title, exercises, athletes }: TrainingSummaryCardProps) {
-    return (
-      <Link href={`trainings/${id}`}>
-        <Card className="mb-2">
-          <CardHeader>
-            <CardTitle>{title}</CardTitle>
-            <CardDescription>{exercises} exercicios</CardDescription>
-            <CardDescription>{athletes} atleta{athletes > 1 ? 's' : ''}</CardDescription>
-          </CardHeader>
-        </Card>
-      </Link>
-    )
+  const router = useRouter();
+
+  async function handleStart(e: MouseEvent<HTMLButtonElement>) {
+    e.stopPropagation();
+    const response = await fetch('http://localhost:3000/training-executions', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        trainingId: id
+      })
+    });
+
+    const trainingExecution = await response.json();
+
+    if (response.ok) {
+      router.push(`training-executions/${trainingExecution.id}`)
+    }
+  }
+
+  function handleCardClick() {
+    router.push(`trainings/${id}`)
+  }
+
+  return (
+    <Card onClick={handleCardClick} className="mb-2 flex justify-between items-center cursor-pointer">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{exercises} exercicios</CardDescription>
+        <CardDescription>{athletes} atleta{athletes > 1 ? 's' : ''}</CardDescription>
+      </CardHeader>
+      <div className="flex justify-center items-center mr-6">
+        <Button onClick={handleStart}>
+          Iniciar ▶️
+        </Button>
+      </div>
+    </Card>
+  )
 }
